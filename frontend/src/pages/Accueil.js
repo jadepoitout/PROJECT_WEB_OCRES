47491068bc4a99b8e135e84cc39c345e47484dba
jadepoitout <poitoutjade@gmail.com>
2022-11-30 18:27:28 +0100

    base navbar et debut responsive

--- frontend/src/pages/Accueil.js
+++ frontend/src/pages/Accueil.js
@@ -1,7 +1,10 @@
+import Navbar from "../components/Navbar";
+
 function Accueil ()
 {
     return(
         <div>
+            <Navbar />
             <h1>ACCUEIL</h1>
         <br />
         <p> J'adore le sport hihi
